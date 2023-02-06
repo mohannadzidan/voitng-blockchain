bc04ba2dc32987347b5f89780740b660f492ed50
Omar Tarek <otareq44@gmail.com>
2023-02-07 00:49:48 +0200

    add icons

---
+++ src/components/icons/Steel.jsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+const Steel = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <path
+        d="m11.945 17.97-7.367-4.348L11.941 24l7.37-10.38-7.37 4.348h.004ZM12.055 0 4.69 12.223l7.364 4.355 7.367-4.35L12.055 0Z"
+        fill="#222145"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default Steel
